efa8805f0f3b79a13c4668a66b63b08bf2c50f23
jiangli <jiangli@able-elec.com>
2017-12-29 17:10:42 +0800

    first

---
+++ src/HelloAction.java
@@ -0,0 +1,28 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+/**
+ * @author Jiangli
+ * @date 2017/12/29 15:55
+ */
+public class HelloAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+        //当前工程对象
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+
+        System.out.println("显示对话框111");
+
+        //显示一个对话框，让用户输入名字
+        String msg = Messages.showInputDialog(project, "你的名字?", "titlehahaha", Messages.getQuestionIcon());
+
+        System.out.println("得到名称:"+msg);
+
+        Messages.showMessageDialog(project,"你好"+msg,"titleaaasdsad",Messages.getInformationIcon());
+    }
+}
